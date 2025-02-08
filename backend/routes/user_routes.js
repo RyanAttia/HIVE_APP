4f7494b1d19ae6b2cd041e91a50e27611e4c25b8
Ryan Attia <ryanattia02@gmail.com>
2025-02-07 20:10:49 -0500

    Update user_routes.js

--- backend/routes/user_routes.js
+++ backend/routes/user_routes.js
@@ -1,9 +1,9 @@
 import express from "express";
 import protectRoute from "../middleware/protectRoute.js";
-import { getUsersForSidebar } from "../controllers/user_controller.js";
+import { get_users_for_sidebar  } from "../controllers/user_controller.js";
 
 const router = express.Router();
 
-router.get("/", protectRoute, getUsersForSidebar);
+router.get("/", protectRoute, get_users_for_sidebar );
 
 export default router;
