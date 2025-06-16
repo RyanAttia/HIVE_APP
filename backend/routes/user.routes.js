5a4db9790cf4a28c8cbc8dbe606f147f941256ae
RyanAttia <ryanattia02@gmail.com>
2025-06-16 00:31:25 -0400

    Production

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -6,8 +6,9 @@ const router = express.Router();
 
 router.get("/", protectRoute, getUsersForSidebar);
 router.put("/status", protectRoute, updateUserStatus);
-router.get("/:id", protectRoute, getUserById);
 router.get("/username/:username", protectRoute, getUserByUsername);
+router.get("/:id", protectRoute, getUserById);
+
 
 
 
--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,9 @@ import messageRoutes from "./routes/message.routes.js";
 import path from "path";
 import { fileURLToPath } from "url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 dotenv.config();
 
 const app = express();
@@ -38,12 +41,10 @@ app.use("/api/conversations", conversationRoutes);
 app.use("/api/messages", messageRoutes);
 
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.get("*", (req, res) => {
+app.get(/^\/(?!api).*/, (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
 });
 
